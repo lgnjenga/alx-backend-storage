134c82581561d49130cc718ae8613140006b71aa
lgnjenga <lgnjenga@gmail.com>
2024-10-17 23:10:00 +0300

    MySQL Advanced Tasks

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,5 @@
+-- creates an index 'idx_name_first' on the table
+-- 'names' and the first letter of 'name'
+DROP INDEX IF EXISTS idx_name_first ON names;
+
+CREATE INDEX idx_name_first ON names(SUBSTRING(name, 1, 1));
